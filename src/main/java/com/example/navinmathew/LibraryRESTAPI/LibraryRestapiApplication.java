dd7c1f87293bbfc217c0f307619bd38d523fb3c9
navin <navin@pop-os.localdomain>
2020-09-19 16:02:03 -0500

    first commit

---
+++ src/main/java/com/example/navinmathew/LibraryRESTAPI/LibraryRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.navinmathew.LibraryRESTAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryRestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/navinmathew/LibraryRESTAPI/LibraryRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.navinmathew.LibraryRESTAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
